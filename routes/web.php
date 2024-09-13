a83dcdc33041832981c774347278c3fd8e1e1651
Abanoub <bebo2xd@gmail.com>
2024-09-14 01:11:05 +0300

    remove upload from from / route

--- routes/web.php
+++ routes/web.php
@@ -9,47 +9,8 @@ use Illuminate\Support\Facades\Storage;
 
 
 Route::get('/', function () {
-    return response()->make('
-        <html>
-        <head>
-            <title>File Upload</title>
-        </head>
-        <body>
-            <form action="/upload" method="POST" enctype="multipart/form-data">
-                <input type="hidden" name="_token" value="' . csrf_token() . '">
-                <label for="file">Select a file:</label>
-                <input type="file" name="file" id="file" required>
-                <br><br>
-                <button type="submit">Upload File</button>
-            </form>
-        </body>
-        </html>
-    ');
-    $user = User::first();
-
-    // $user->deposit(2);
-    // $user->withdraw(7);
-
-    return $user->transactions;
-    return $user->purchases;
-
     return view('welcome');
 });
-
-Route::post('/upload', function (Request $request) {
-    if (!$request->hasFile('file')) {
-        return response()->json(['error' => 'No file provided'], 400);
-    }
-
-    $file = $request->file('file');
-    if (!$file->isValid()) {
-        return response()->json(['error' => 'Invalid file'], 400);
-    }
-
-    $path = $file->store('uploads', 'public');
-    return response()->json(['path' => $path], 200);
-});
-
 
 
 ////////////////

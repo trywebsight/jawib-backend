b2fa8f2a5828d8795de8e1bfd81c34420b5ad6ac
MarwaEzzatMo <marwaezzat201@gmail.com>
2024-09-14 01:16:04 +0000

    fix images issue

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+	    $middleware->alias([
+            'setLocale' => SetLocale::class,
+        ]);
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {

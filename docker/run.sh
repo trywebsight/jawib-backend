9665c73a13ddbf7ca78c5b681775bf02e0cb0101
Abanoub <bebo2xd@gmail.com>
2024-09-15 11:59:03 +0300

    append optimize commands to run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -10,4 +10,7 @@ chmod 777 /var/www/bootstrap/cache
 php artisan queue:work &
 php artisan migrate --force
 
+php artisan optimize
+php artisan filament:optimize
+
 /usr/bin/supervisord -c /etc/supervisord.conf

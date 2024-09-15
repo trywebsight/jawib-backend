62d2922a31b967e59a178e24565f56366a882997
Abanoub <bebo2xd@gmail.com>
2024-09-15 12:01:33 +0300

    separate route middlewares

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append([
+        $middleware->api(append: [
             App\Http\Middleware\ApiLocalization::class,
+        ]);
+        $middleware->web(append: [
             App\Http\Middleware\AlwaysValidSignature::class,
         ]);
         $middleware->trustProxies(at: '*');

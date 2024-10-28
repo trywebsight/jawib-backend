6ce6dd2b77733767367acfa4138af4cc6ccc95f2
Abanoub <bebo2xd@gmail.com>
2024-10-28 16:05:54 +0300

    Store migrations

---
+++ database/migrations/2024_10_28_094233_create_store_order_items_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('store_order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('store_orders')->cascadeOnDelete();
+            $table->foreignId('product_id')->constrained('store_products')->cascadeOnDelete();
+            $table->integer('quantity')->default(1);
+            $table->decimal('price', 10, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('store_order_items');
+    }
+};

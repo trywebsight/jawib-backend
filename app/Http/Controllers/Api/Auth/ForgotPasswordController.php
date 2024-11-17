24a630ddcb11fb66d467f77650c2bdd3d969372e
bebo2xd <bebo2xd@gmail.com>
2024-11-17 11:40:09 +0200

    wait 60 seconds before resend otp

--- app/Http/Controllers/Api/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Api/Auth/ForgotPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Models\UserOtp;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Validator;
@@ -23,6 +24,15 @@ class ForgotPasswordController extends Controller
         }
 
         $user = User::where('phone', $request->phone)->first();
+
+        $existOtp = UserOtp::where('user_id', $user->id)->first();
+        if ($existOtp && $existOtp->updated_at) {
+            $lastOtpSentAt = Carbon::parse($existOtp->updated_at);
+            // Allow sending a new OTP only if more than 60 seconds have passed
+            if ($lastOtpSentAt->diffInSeconds(Carbon::now()) < 60) {
+                return $this->error([], __("please wait :seconds seconds before requesting a new otp", ['seconds' => intval(60 - $lastOtpSentAt->diffInSeconds(Carbon::now()))]), 422);
+            }
+        }
 
         // Generate a 4-digit OTP
         // $otp = rand(1000, 9999);
